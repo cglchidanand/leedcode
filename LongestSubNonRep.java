bb7cfe6c65bea6d0cb156345c804a92b25c2bf5b
cglchidanand <36496582+cglchidanand@users.noreply.github.com>
2024-10-24 11:01:37 +0530

    Add files via upload

---
+++ LongestSubNonRep.java
@@ -0,0 +1,35 @@
+package com.example.demo;
+
+import java.util.*;
+
+public class LongestSubNonRep {
+    
+    static int longestUniqueSubstr(String s) {
+        int n = s.length();
+        int res = 0;
+
+        for (int i = 0; i < n; i++) {
+
+             boolean[] visited = new boolean[256];
+
+            for (int j = i; j < n; j++) {
+
+                 if (visited[s.charAt(j)]) {
+                    break;
+                } 
+                  else {
+                  
+                   
+                    res = Math.max(res, j - i + 1);
+                    visited[s.charAt(j)] = true;
+                }
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        String s = "geeksforgeeks";
+        System.out.println(longestUniqueSubstr(s));
+    }
+}
