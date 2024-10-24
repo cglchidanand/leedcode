832e8daeb03663f16d368c26d539a04f41ada18a
cglchidanand <36496582+cglchidanand@users.noreply.github.com>
2024-10-24 11:07:15 +0530

    Add files via upload

---
+++ QueueSlidingWindow.java
@@ -0,0 +1,45 @@
+package com.example.demo;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class QueueSlidingWindow {
+    public static int[] minSlidingWindow(int[] nums, int k) {
+        if (nums == null || nums.length == 0 || k <= 0) {
+            return new int[0];
+        }
+
+        int n = nums.length;
+        int[] result = new int[n - k + 1];
+        Deque<Integer> deque = new ArrayDeque<>();
+
+        for (int i = 0; i < n; i++) {
+             if (!deque.isEmpty() && deque.peekFirst() < i - k + 1) {
+                deque.pollFirst();
+            }
+
+             while (!deque.isEmpty() && nums[deque.peekLast()] > nums[i]) {
+                deque.pollLast();
+            }
+
+             deque.offerLast(i);
+
+             if (i >= k - 1) {
+                result[i - k + 1] = nums[deque.peekFirst()];
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {9, 1, 3, -1, 5, 3, 6, 7};
+        int k = 3;
+        int[] result = minSlidingWindow(nums, k);
+
+         for (int min : result) {
+            System.out.print(min + " ");
+        }
+    }
+}
