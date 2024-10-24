0d5af10ac87e20e19a4abfbb734b3c243bdb781c
cglchidanand <36496582+cglchidanand@users.noreply.github.com>
2024-10-24 11:09:26 +0530

    Add files via upload

---
+++ SlidingWindowMinimum.java
@@ -0,0 +1,47 @@
+package com.example.demo;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class SlidingMinimum {
+    public static int[] min(int[] nums, int k) {
+
+        int n = nums.length;
+        int[] result = new int[n - k + 1];
+        Deque<Integer> deque = new ArrayDeque<>();
+        
+        if (nums == null || nums.length == 0 || k <= 0) {
+            return new int[0];
+        }
+
+
+        for (int i = 0; i < n; i++) {
+            if (!deque.isEmpty() && deque.peekFirst() < i - k + 1) {
+                deque.pollFirst();
+            }
+
+            while (!deque.isEmpty() && nums[deque.peekLast()] > nums[i]) {
+                deque.pollLast();
+            }
+
+            deque.offerLast(i);
+
+            if (i >= k - 1) {
+                result[i - k + 1] = nums[deque.peekFirst()];
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {9, 1, 3, -1, 5, 3, 6, 7};
+        int k = 3;
+        int[] result = min(nums, k);
+
+        for (int min : result) {
+            System.out.print(min + " ");
+        }
+    }
+}
