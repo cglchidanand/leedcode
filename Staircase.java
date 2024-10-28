be4ffcc28f2ed885c8541b787689ea8a953a00da
cglchidanand <36496582+cglchidanand@users.noreply.github.com>
2024-10-28 14:38:40 +0530

    Create Staircase.java

---
+++ Staircase.java
@@ -0,0 +1,21 @@
+public class Staircase {
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.println("Number of ways to reach the " + n + "th stair: " + countWays(n)); // Output: 8
+    }
+
+    public static int countWays(int n) {
+        if (n == 0) return 1; 
+        if (n == 1) return 1; 
+
+         int[] dp = new int[n + 1];
+        dp[0] = 1; 
+        dp[1] = 1; 
+
+         for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        return dp[n];
+    }
+}
